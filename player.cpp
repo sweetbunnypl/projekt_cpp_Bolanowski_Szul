af22dbc50579dabab7e0814cb40436896b648890
sweetbunnypl <karol.bol3@gmail.com>
2020-10-23 18:45:14 +0200

    visual version

---
+++ player.cpp
@@ -0,0 +1,27 @@
+#include "player.h"
+
+Player::Player()
+{
+
+}
+
+Player::~Player()
+{
+
+}
+
+int Player::movement(sf::Vector2f coordinates){
+    return 0;
+};
+
+int Player::health(int player_health){
+    return 0;
+};
+
+int Player::pointsSystem(int points){
+    return 0;
+};
+
+void Player::player_death(){
+
+};
